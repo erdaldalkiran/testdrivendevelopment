73d1cb35ddca97b3134305b89f46af82aaaed9a5
erdalkiran <erdalkiran@gmail.com>
2014-06-24 12:13:29 +0300

    CH14

--- TestDrivenDevelopment/Domain/Bank.cs
+++ TestDrivenDevelopment/Domain/Bank.cs
@@ -5,46 +5,56 @@
 
     public sealed class Bank
     {
-        private Hashtable Rates = new Hashtable();
-        public Money Reduce(IExpression source, Currency currency)
-        {
-            return source.Reduce(this, currency);
-        }
+        #region Fields
 
-        public decimal Rate(Currency from, Currency to)
-        {
-            if (from == to) return 1;
-            return Convert.ToDecimal(Rates[new Pair(from, to)]);
-        }
+        private readonly Hashtable Rates = new Hashtable();
+
+        #endregion
+
+        #region Public Methods and Operators
 
         public void AddRate(Currency from, Currency to, decimal rate)
         {
             Rates.Add(new Pair(from, to), rate);
         }
 
+        public decimal Rate(Currency from, Currency to)
+        {
+            if (from == to)
+            {
+                return 1;
+            }
+            return Convert.ToDecimal(Rates[new Pair(from, to)]);
+        }
+
+        public Money Reduce(IExpression source, Currency currency)
+        {
+            return source.Reduce(this, currency);
+        }
+
+        #endregion
+
         private sealed class Pair
         {
-            private bool Equals(Pair other)
-            {
-                return this.From == other.From && this.To == other.To;
-            }
-
-            public override int GetHashCode()
-            {
-                unchecked
-                {
-                    return ((int)this.From * 397) ^ (int)this.To;
-                }
-            }
+            #region Constructors and Destructors
 
             internal Pair(Currency from, Currency to)
             {
                 From = from;
                 To = to;
             }
+
+            #endregion
+
+            #region Properties
+
             private Currency From { get; set; }
 
             private Currency To { get; set; }
+
+            #endregion
+
+            #region Public Methods and Operators
 
             public override bool Equals(object obj)
             {
@@ -58,6 +68,25 @@
                 }
                 return obj is Pair && Equals((Pair)obj);
             }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return ((int)this.From * 397) ^ (int)this.To;
+                }
+            }
+
+            #endregion
+
+            #region Methods
+
+            private bool Equals(Pair other)
+            {
+                return this.From == other.From && this.To == other.To;
+            }
+
+            #endregion
         }
     }
 }
