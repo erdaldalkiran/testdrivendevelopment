b5b275260b8f7fe0416adb578454f67e1b789f86
erdalkiran <erdalkiran@gmail.com>
2014-06-25 11:41:52 +0300

    Second Edtion Ch4

---
+++ TestDrivenDevelopment/MoneyExample/Domain.Tests/MoneyTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MoneyTests
+    {
+
+        [TestMethod]
+        public void Multiplication_MultiplyWith2_DoubledAmount()
+        {
+            var fiveDollar = new Dollar(5);
+            
+            Assert.AreEqual(new Dollar(10), fiveDollar.Times(2));
+            Assert.AreEqual(new Dollar(5), fiveDollar);
+        }
+
+
+        [TestMethod]
+        public void Equals_CompareWithSameAmount_MustBeEqual()
+        {
+            Assert.AreEqual(new Dollar(5), new Dollar(5));
+        }
+
+        [TestMethod]
+        public void Equals_CompareWithDifferentAmount_NotEqual()
+        {
+            Assert.AreNotEqual(new Dollar(5), new Dollar(6));
+        }
+
+    }
+}
---
+++ TestDrivenDevelopment/MoneyExample/Domain/Dollar.cs
@@ -0,0 +1,43 @@
+﻿namespace Domain
+{
+    public class Dollar
+    {
+        protected bool Equals(Dollar other)
+        {
+            return this._amount == other._amount;
+        }
+
+        public override int GetHashCode()
+        {
+            return this._amount.GetHashCode();
+        }
+
+        public Dollar(decimal amount)
+        {
+            this._amount = amount;
+        }
+
+        private decimal _amount { get; set; }
+
+        public Dollar Times(int multiplier)
+        {
+            return new Dollar(_amount * multiplier);
+        }
+
+
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            
+            return obj is Dollar && _amount == (obj as Dollar)._amount;
+        }
+    }
+}
